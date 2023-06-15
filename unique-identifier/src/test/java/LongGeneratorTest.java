4bf55f66d0121af07463be147e1099846ac7388d
kaushik ganguly <kgsnipes@gmail.com>
2023-06-15 08:36:07 -0400

    adding more tests

--- unique-identifier/src/test/java/LongGeneratorTest.java
+++ unique-identifier/src/test/java/LongGeneratorTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.uid.impl.LongGenerator;
 
+import java.util.stream.IntStream;
+
 public class LongGeneratorTest {
 
     LongGenerator longGenerator;
@@ -12,5 +14,45 @@ public class LongGeneratorTest {
     {
         longGenerator=new LongGenerator();
         Assertions.assertEquals(1L,longGenerator.getNext());
+    }
+
+    @Test
+    public void longGeneratorCountTest()
+    {
+        longGenerator=new LongGenerator();
+        IntStream.range(0,10).forEach(num->longGenerator.getNext());
+        Assertions.assertEquals(11L,longGenerator.getNext());
+    }
+
+    @Test
+    public void longGeneratorHighCountTest()
+    {
+        longGenerator=new LongGenerator();
+        IntStream.range(0,10000).forEach(num->longGenerator.getNext());
+        Assertions.assertEquals(10001L,longGenerator.getNext());
+    }
+
+    @Test
+    public void longGeneratorWithStartValueTest()
+    {
+        longGenerator=new LongGenerator(100L);
+        IntStream.range(0,1000).forEach(num->longGenerator.getNext());
+        Assertions.assertEquals(1100L,longGenerator.getNext());
+    }
+
+    @Test
+    public void longGeneratorWithStepValueTest()
+    {
+        longGenerator=new LongGenerator(100L,2);
+        IntStream.range(0,1000).forEach(num->longGenerator.getNext());
+        Assertions.assertEquals(2100L,longGenerator.getNext());
+    }
+
+    @Test
+    public void longGeneratorWithNegativeStepValueTest()
+    {
+        longGenerator=new LongGenerator(100L,-1);
+        IntStream.range(0,10).forEach(num->longGenerator.getNext());
+        Assertions.assertEquals(90L,longGenerator.getNext());
     }
 }
